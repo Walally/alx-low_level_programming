94c778790b05b93482dd26164ec89cd844f400ef
Walally <bawata29ally@gmail.com>
2023-02-16 21:21:35 +0000

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - print to screen
  * Return: zero
  */
-
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
 
