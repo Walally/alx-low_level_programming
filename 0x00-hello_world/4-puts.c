93b58fb8e992bf12de31d04e2f721e64d01655f9
Walally <bawata29ally@gmail.com>
2023-02-16 20:53:42 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,19 @@
 #!/bin/bash
 #include <stdio.h>
 
-int main(void) {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+/**
+ *  * main - print to screen
+ *
+ *  *
+ *
+ *  * Return: zero
+ *
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
 }
