331350ce4260c7fbcc4d90ebc6b05487895d2c35
Walally <bawata29ally@gmail.com>
2023-02-18 03:42:41 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,12 @@
  * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int n;
-
-	for (n = 0; n < 10; n++)
+int main(void) {
+	int n = 0;
+	while(n < 10) {
 		printf("%d", n);
-	printf("\n");
+		printf("\n");
+		n++;
+	}
 	return (0);
 }
