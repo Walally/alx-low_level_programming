1fc05fa8a1587bd5751a0c608ead0e03033bff42
Walally <bawata29ally@gmail.com>
2023-02-19 03:24:44 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 			if (x < y)
 			{
 				putchar((x / 10) + 48);
-				putchar((y % 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
 				putchar((y / 10) + 48);
 				putchar((y % 10) + 48);
