f1058ab5966f78254ad0f9142e520567dcce6cfb
Walally <bawata29ally@gmail.com>
2023-03-01 16:38:09 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies characters from one string to another
+ * @n: maximum number of characters to copy from 'src' to 'dest'
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+		return (dest);
+}
